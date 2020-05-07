fe6b7c4718225774d29aaa2e13253ab04f4acffa
lars <lars.scheme@gmail.com>
2020-05-07 09:49:44 +0200

    Add explanation

--- tester.go
+++ tester.go
@@ -1,3 +1,22 @@
+// This demonstrates godoc.org issue #657.
+//
+// 	https://github.com/golang/gddo/issues/657
+//
+// Formatting of the Do function looks like this
+//
+// 	func Do( a int, b string, )
+//
+// where it should rather be
+//
+// 	func Do(a int, b string)
+//
+// This bug shows when a function is formatted with its parameters on extra
+// lines like this:
+//
+// 	func Do(
+// 		a int,
+// 		b string,
+// 	) {}
 package tester
 
 func Do(
